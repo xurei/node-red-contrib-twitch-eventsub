0ca4a556a9677b6c26023045eb6d96aa4ed14287
xurei <xureilab@gmail.com>
2024-05-12 23:29:36 +0200

    🏗️ Converted node to typescript

--- scripts/copy-static-assets.js
+++ scripts/copy-static-assets.js
@@ -3,5 +3,4 @@ const shell = require('shelljs');
 shell.mkdir("-p", "dist");
 shell.mkdir("-p", "dist/icons");
 shell.cp("-R", "src/*.html", "dist/");
-shell.cp("-R", "src/*.js", "dist/");
 shell.cp("-R", "src/icons/*.svg", "dist/icons/");
--- src/twitch-eventsub.ts
+++ src/twitch-eventsub.ts
@@ -1,34 +1,35 @@
 module.exports = function(RED) {
   function EventSubNode(config) {
-    RED.nodes.createNode(this, config);
-    const node = this;
-    
+    //@ts-expect-error any
+    const node = this as any;
+    RED.nodes.createNode(node, config);
+
     const id = Math.floor(Math.random()*1000000);
-  
-    this.twitchConfig = RED.nodes.getNode(config.config);
-  
-    if (this.twitchConfig) {
+
+    node.twitchConfig = RED.nodes.getNode(config.config);
+
+    if (node.twitchConfig) {
       // On Start
-      this.twitchConfig.addNode(id, node);
-  
+      node.twitchConfig.addNode(id, node);
+
       // On Delete
-      this.on('close', function() {
-        this.twitchConfig.removeNode(id);
+      node.on('close', function() {
+        node.twitchConfig.removeNode(id);
       });
     }
     else {
       // No config node configured
       // TODO show error message if no config found
     }
-    
+
     node.triggerTwitchEvent = function (event) {
       node.send({
         payload: event
       });
     };
   }
-  
+
   EventSubNode.icon = 'twitch-icon.png';
-  
+
   RED.nodes.registerType("twitch-eventsub", EventSubNode);
 }
